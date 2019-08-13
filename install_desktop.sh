54a5d6963e6d2376d001f6e8dcf3efcf3855d73a
Harry Collins <hcollins345@gmail.com>
2019-08-13 16:02:57 +0100

    Update install_desktop.sh

--- install_desktop.sh
+++ install_desktop.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-echo '==== Upgrade system ===='
-sudo apt-get update
-sudo apt-get upgrade -y
-
-echo '==== Install packages ===='
 sudo apt-get install -y gnome-shell
-sudo apt-get install -y ubuntu-gnome-desktop
+sudo apt-get install -y ubuntu-desktop
+#sudo apt-get install -y ubuntu-gnome-desktop
 sudo apt-get install -y autocutsel
-sudo apt-get install -y tightvncserver
+#sudo apt-get install -y tightvncserver
 sudo apt-get install -y gnome-core
 sudo apt-get install -y gnome-panel
 sudo apt-get install -y synaptic
